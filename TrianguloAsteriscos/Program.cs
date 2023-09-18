3dd364f2e9886d94a6b06fa79e1fcdb0d3626bbf
DarkHeartJ <jair_delcastillo@hotmail.com>
2023-09-18 12:08:41 -0600

    Agregar archivos de proyecto.

---
+++ TrianguloAsteriscos/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TrianguloAsteriscos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.WriteLine("Introduce la altura del triangulo: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
